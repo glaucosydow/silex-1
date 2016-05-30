bb4b6f479d80d77732bfb3447c1e70ab34a8d96a
danilobossanova <danilo.bossanova@hotmail.com>
2016-05-30 15:47:18 -0300

    Retornando cliente em Json

--- public/index.php
+++ public/index.php
@@ -6,5 +6,25 @@
  * Time: 14:47
  */
 
-require_once '../bootstrap.php';
+require_once  __DIR__ . '/../bootstrap.php';
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+
+$app->get('/', function(){
+    $cliente = [
+        0 => ['nome' => 'Cliente Teste 00', 'email' => 'email@teste.com.br', 'CPF' => '000.000.000-00'],
+        1 => ['nome' => 'Cliente Teste 01', 'email' => 'email@teste.com.br', 'CPF' => '000.000.000-00'],
+        2 => ['nome' => 'Cliente Teste 02', 'email' => 'email@teste.com.br', 'CPF' => '000.000.000-00'],
+    ];
+
+    return JsonResponse::create($cliente,200,['Content-Type' =>'application/json']);
+
+});
+
+
+/**
+ * Roda nossa aplicação
+ */
+$app->run();
